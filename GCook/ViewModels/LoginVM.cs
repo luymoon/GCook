bbeddb7ef0e661502fcccc1c240370025f1c37c1
luymoon <aguiarluana780@gmail.com>
2024-09-18 14:45:52 -0300

    Dia 18-09-24

--- GCook/ViewModels/LoginVM.cs
+++ GCook/ViewModels/LoginVM.cs
@@ -4,13 +4,13 @@ namespace GCook.ViewModels;
 
 public class LoginVM
 {
-    [Display(Name = "Email ou Nome de Usuário", Prompt = "Email ou Nome de Usuário")]
+    [Display(Name = "Email ou Nome de Usuário", Prompt = "Informe seu Email ou Nome de Usuário")]
     [Required(ErrorMessage = "Por favor, informe seu email ou nome de usuário")]
     public string Email { get; set; }
 
-    [DataType(DataType.Password)]
-    [Display(Name = "Senha de Acesso", Prompt = "Senha de Acesse")]
+    [Display(Name = "Senha de Acesso", Prompt = "********")]
     [Required(ErrorMessage = "Por favor, informe sua senha de acesso")]
+    [DataType(DataType.Password)]
     public string Senha { get; set; }
 
     [Display(Name = "Manter Conectado?")]
